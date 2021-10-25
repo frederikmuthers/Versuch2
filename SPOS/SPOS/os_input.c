6d0e4c5a4ce090135c18bd1db86f2f346101403b
JakobKoempel <jakob.koempel@gmail.com>
2021-10-25 12:23:36 +0200

    Button INput

--- SPOS/SPOS/os_input.c
+++ SPOS/SPOS/os_input.c
@@ -18,27 +18,53 @@ Everything that is necessary to get the input from the Buttons in a clean format
  *           4 Buttons: 1,3,4 -pushed: 000001101
  *
  */
+/*
+	Belegung des PortsC
+	C0 Enter
+	C1 Down
+	C2 JTAG
+	C3 JTAG
+	C4 JTAG
+	C5 JTAG
+	C6 Up
+	C7 ESC
+*/
 uint8_t os_getInput(void) {
-    #warning IMPLEMENT STH. HERE
+	//nicht an den viert Mittleren Pins interessiert
+	uint8_t  a = PINC & 0b11000011;
+	//extrahiere zwei höchsten Buts (Button Up und ESC)
+	uint8_t b = a & 0b11000000;
+	//shifte diese Bits an die Stelle Nummer 2 und 3
+	b = b >> 4;
+	//Füge diese Bits der Rückgabe wieder hinzu
+	a |= b;
+	return a; 
 }
 
 /*!
  *  Initializes DDR and PORT for input
  */
 void os_initInput() {
-    #warning IMPLEMENT STH. HERE
+	//setze C0, C1, C6, C7 als Input, verändere andere Pins nicht
+    DDRC &= 0b00111100;
+	//setze Pullup Widerstände für C0, C1, C6, C7, verändere andere Pins nicht
+	PORTC |= 0b11000011;
 }
 
 /*!
  *  Endless loop as long as at least one button is pressed.
  */
 void os_waitForNoInput() {
-    #warning IMPLEMENT STH. HERE
+    while(os_getInput() != 0){
+		//warte
+	}
 }
 
 /*!
  *  Endless loop until at least one button is pressed.
  */
 void os_waitForInput() {
-    #warning IMPLEMENT STH. HERE
+    while(os_getInput() == 0){
+		//warte
+	}
 }
